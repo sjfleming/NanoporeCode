00c2b2e20df1f9b5dfcb79698647070411f142c8
sjfleming <sjf11@case.edu>
2016-10-18 19:15:28 -0400

    analysis.findEventRegions now works

--- PoreView/analysis.m
+++ PoreView/analysis.m
@@ -66,73 +66,111 @@ classdef analysis < handle
             [voltage, current] = obj.getViewData(in.trange);
             dt = diff(in.trange)/numel(current);
             voltagelogic = obj.findSpecifiedVoltageRegions(in.trange, in.voltage);
-            voltage = medfilt1(voltage(voltagelogic), 10); % limit our analysis to sections with specified voltage(s)
-            current = medfilt1(current(voltagelogic), 2); % median filter by 2 to get rid of spurious minmax downsampling artefacts
+            voltage = medfilt1(voltage, 10); % limit our analysis to sections with specified voltage(s)
+            voltage(~voltagelogic) = NaN;
+            current(~voltagelogic) = NaN;
             conductance = current./voltage;
-            V = mode(round(voltage(voltage>max(voltage)/2))); % capture voltage assumed to be most prevalent overall high voltage value
+            clear current;
+            v_with_regions_deleted = voltage(voltagelogic);
+            clear voltagelogic;
+            V = mode(round(v_with_regions_deleted(v_with_regions_deleted>nanmax(v_with_regions_deleted)/2))); % capture voltage assumed to be most prevalent overall high voltage value
+            clear v_with_regions_deleted;
             if strcmp(in.eventstart, 'currentdrop')
                 lowcond = conductance < g_m * in.threshhold; % regions of conductance below threshhold
-                dlogic = diff([0; lowcond; 0]);
+                dlogic = diff([1; lowcond; 0]);
                 startcondition = @(x) x(:,2)*1000./x(:,3) < g_m * in.threshhold;
+                startcondition_inv = @(x) x(:,2)*1000./x(:,3) > g_m * in.threshhold;
                 endcondition = @(x,evtcond) x(:,2)*1000./x(:,3) < g_m * in.threshhold;
             elseif strcmp(in.eventstart, 'voltagedrop')
                 lowvolt = voltage < V * in.threshhold; % regions of voltage below threshhold
-                dlogic = diff([0; lowvolt; 0]);
+                dlogic = diff([1; lowvolt; 0]);
                 startcondition = @(x) x(:,3) < V * in.threshhold;
+                startcondition_inv = @(x) x(:,3) > V * in.threshhold;
                 endcondition = @(x,evtcond) x(:,2)*1000./x(:,3) < g_m * in.threshhold;
             end
-            possibleStartInds = find(dlogic > 0);
-            possibleEndInds = find(dlogic < 0) - 1;
-            if possibleEndInds(1)<possibleStartInds(1)
-                % if there's an end with no start, get rid of it
-                possibleEndInds = possibleEndInds(2:end);
-            end
-            if numel(possibleEndInds) < numel(possibleStartInds)
-                % if insufficient end indices found, just equate to starts
-                possibleEndInds(end+1:end+numel(possibleStartInds)-numel(possibleEndInds)-1) = possibleStartInds(numel(possibleEndInds)+1:end);
-            end
+            clear voltage;
+            [~,possibleStartInds] = findpeaks(double(dlogic > 0),'minpeakheight',0.5,'minpeakdist',5);
+            % check to make sure current starts at open pore level
+            startsHigh = arrayfun(@(x) nanmax(conductance(x-5:x)) > g_m * in.threshhold, possibleStartInds);
+            possibleStartInds = possibleStartInds(startsHigh);
+            % one end for each start
+            possibleEndInds = arrayfun(@(x) find(or(conductance(x+2:end) > g_m * in.threshhold, isnan(conductance(x+2:end))), 1, 'first'), possibleStartInds) + possibleStartInds + 1;
             % to get rid of the off-by-one errors
-            possibleStartInds = possibleStartInds - 2;
-            possibleEndInds = possibleEndInds - 2;
+            possibleStartInds = possibleStartInds - 1;
             
             % exact start and end search
             start_inds = -1*ones(numel(possibleStartInds),1);
             end_inds = start_inds;
             pad = -2;
             for i = 1:numel(possibleStartInds) % go through all candidates
+                % use a double-finding scheme to be as robust as possible
                 % find start
-                start_inds(i) = obj.sigdata.findNext(@(x) startcondition(x), ...
+                temp_start = obj.sigdata.findPrev(@(x) startcondition_inv(x), ...
                     (in.trange(1)/dt + min(possibleStartInds(i)+pad, possibleEndInds(i))) * dt/obj.sigdata.si);
+                start_inds(i) = obj.sigdata.findNext(@(x) startcondition(x), temp_start-1);
                 % get mean event conductance
                 evtconductancearray = conductance(possibleStartInds:possibleEndInds);
                 evtconductance = mean( evtconductancearray(evtconductancearray < mean([g_m, min(evtconductancearray)])) );
                 % find end by next cross of threshhold
                 if possibleEndInds(i)-possibleStartInds(i) < pad*dt/obj.sigdata.si
                     % end is so close we can search from the start
-                    end_inds_thresh = obj.sigdata.findPrev(@(x) endcondition(x, evtconductance), start_inds(i)+pad);
+                    end_inds_thresh = obj.sigdata.findNext(@(x) or(x(:,2)*1000./x(:,3) > g_m * in.threshhold, x(:,3)<=0), start_inds(i)+pad);
+                    end_inds_thresh = obj.sigdata.findPrev(@(x) or(endcondition(x, evtconductance), x(:,3)<=0), end_inds_thresh+1);
                 else
                     % end is far enough that we should work from our
                     % best guess of the end itself
-                    end_inds_thresh = obj.sigdata.findPrev(@(x) endcondition(x, evtconductance), ...
+                    end_inds_thresh = obj.sigdata.findNext(@(x) or(x(:,2)*1000./x(:,3) > g_m * in.threshhold, x(:,3)<=0), ...
                         (in.trange(1)/dt + max(possibleEndInds(i)-pad, possibleStartInds(i))) * dt/obj.sigdata.si+1);
+                    end_inds_thresh = obj.sigdata.findPrev(@(x) or(endcondition(x, evtconductance), x(:,3)<=0), end_inds_thresh+1);
                 end
                 % end should actually be when current starts to return
                 % to open pore
-                ending_bit = obj.sigdata.get(max(start_inds(i),end_inds_thresh-10):max(start_inds(i),end_inds_thresh-3),2)*1000;
+                ending_bit = obj.sigdata.get(max(start_inds(i),end_inds_thresh-20):max(start_inds(i),end_inds_thresh-5),2)*1000;
                 end_cond = mean(ending_bit);
                 end_cond_std = std(ending_bit);
-                end_inds(i) = obj.sigdata.findPrev(@(x) x(:,2)*1000./x(:,3) < end_cond+end_cond_std, end_inds_thresh);
+                end_inds(i) = obj.sigdata.findPrev(@(x) x(:,2)*1000./x(:,3) < end_cond + 0.2*end_cond_std, end_inds_thresh);
                 % make sure we get some ending, if that technique
                 % didn't work
                 if isempty(end_inds(i))
                     end_inds(i) = end_inds_thresh;
                     display('problem identifying exact event end')
                 end
+                % make sure this doesn't overlap previous event
+                if i>1
+                    if start_inds(i)<end_inds(i-1)
+                        start_inds(i) = NaN;
+                        end_inds(i) = NaN;
+                    end
+                end
             end
+            start_inds = start_inds(~isnan(start_inds));
+            end_inds = end_inds(~isnan(end_inds));
             
             % give the indices of the regions as output
             regions = [start_inds, end_inds]-1; % fix off-by-one from 'find'
             %regions = [possibleStartInds*dt/obj.sigdata.si, possibleEndInds*dt/obj.sigdata.si]+obj.tr(1)/obj.sigdata.si;
+        end
+        
+        function showEventsInPoreView(obj, pv, r, how)
+            % necessary inputs: PoreView object, result of
+            % findEventRegions, how ('all' or 'one')
+            % plots the events, one-by-one, in PoreView
+            pv.clearAxes();
+            if strcmp(how,'all')
+                for i = 1:size(r,1)
+                    cond(i,1) = pv.data.get(r(i,1),2)*1000/pv.data.get(r(i,1),3);
+                    cond(i,2) = pv.data.get(r(i,2),2)*1000/pv.data.get(r(i,2),3);
+                end
+                plot(r(:,1)*pv.data.si,cond(:,1),'go')
+                plot(r(:,2)*pv.data.si,cond(:,2),'rx')
+            elseif strcmp(how,'one')
+                for i = 1:size(r,1)
+                    pv.setView(max(0,sort(r(i,:)+[-100, 100])).*pv.data.si);
+                    plot(r(i,1)*pv.data.si,pv.data.get(r(i,1),2)*1000/pv.data.get(r(i,1),3),'go')
+                    plot(r(i,2)*pv.data.si,pv.data.get(r(i,2),2)*1000/pv.data.get(r(i,2),3),'rx')
+                    pause();
+                end
+            end
         end
         
     end
