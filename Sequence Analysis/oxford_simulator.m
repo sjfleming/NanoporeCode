efeb6d595376c807cb3db22f0ca6121eb22311d9
sjfleming <sjf11@case.edu>
2017-06-06 15:30:07 -0400

    oxford_simulator: minor update to include standard deviations

--- Sequence Analysis/oxford_simulator.m
+++ Sequence Analysis/oxford_simulator.m
@@ -1,4 +1,4 @@
-function pA = oxford_simulator(seq,model,plotFlag)
+function [pA, pAstd] = oxford_simulator(seq,model,plotFlag)
 
 % Takes in a sequence in the form of 'ATCGTTCAAAGC...' and plots what
 % Oxford's simulated squiggles would look like.
@@ -9,12 +9,13 @@ states = get_states(nt2int(seq), 5); % k=5 for k-mer
 % get the current from Oxford's models
 load('models.mat')
 pA = model_data{model}.level_mean(states);
+pAstd = model_data{model}.sd_mean(states); % standard deviation of the mean
 
 % plot it
 if plotFlag==1
     figure(2)
     clf(2)
-    plot(1:numel(pA),pA,'o-');
+    plot(1:numel(pA),pA,'o');
     ylim([min(model_data{model}.level_mean) max(model_data{model}.level_mean)])
     title(seq,'FontSize',16,'FontName','Courier')
     xlabel('Base Number')
@@ -23,7 +24,8 @@ if plotFlag==1
     set(gca,'FontSize',16)
     set(gca,'LooseInset',[0 0 0 0]) % the all-important elimination of whitespace!
     set(gca,'OuterPosition',[0.01 0.01 0.98 0.98]) % fit everything in there
-    set(gcf,'Position',[-1034 431 1021 278])
+    %set(gcf,'Position',[-1034 431 1021 278])
+    set(gcf,'Position',[113,456,1360,420]);
 end
 
 end
