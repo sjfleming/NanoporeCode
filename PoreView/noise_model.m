532448442a0cd6d2677264e788f9910779cd603e
sjfleming <sjf11@case.edu>
2017-07-28 18:39:54 -0400

    Updates to noise stuff

---
+++ PoreView/noise_model.m
@@ -0,0 +1,35 @@
+function y = noise_model(x,temp,conductance,current,filter,Ra,Cm,loss_tangent)
+% noise_model is a full workup of nanopore noise sources
+% Stephen Fleming
+% 7/28/17
+
+% typical input values:
+% temp = 22; % in Celsius
+% conductance = 2; % in nS
+% Ra = 3e7; % access resistance = rho/4*a (J.E. Hall, 1975), rho = 0.0895ohm*m for 1M KCl
+% Cm = 0.45e-12; % typical membrane capacitance, in Farads
+% loss_tangent = 1; % Dave ref to http://pubs.acs.org/doi/pdf/10.1021/jp0680138
+% current = 1e-9; % in amperes
+
+    R = 1/(conductance*1e-9);
+    k = 1.38 * 10^-23; % Boltzmann constant
+    T = temp + 273.15; % absolute temperature in Kelvin
+    Rf = 500e6; % feedback resistor in Axopatch with beta = 1 in whole cell mode
+    Cin = 4e-12; % headstage input capacitance is about 4pF (Sakmann and Neher say 15pF, p.112)
+    V_headstage = 3e-9; % input voltage noise on headstage op-amp = 3nV/sqrt(Hz)
+    dielectric_loss = @(f) 8*pi*k*T*f*Cm*loss_tangent * 1e18;
+    ion = 1.12e-9 * (current*1e-12)^2 * (1e9)^2; % ion number fluctuations proportional to current squared (10/20/16 data for MspA from Oxford)
+    % Y = @(f,Ra,Cm) sqrt(-1)*2*pi*f*Cin + 1./( 2*Ra + 1./( 1/R + sqrt(-1)*2*pi*f.*Cm ) ) + 1./Rf; % complex conductance (admittance)
+    Y = @(f,Ra,Cm) 1./( 2*Ra + 1./( 1/R + sqrt(-1)*2*pi*f.*Cm ) ) + 1./Rf; % complex conductance (admittance)
+    % headstage = V_headstage^2.*real(Y(f,Ra,Cm).*conj(Y(f,Ra,Cm))) * 10^18;
+    headstage = @(f) V_headstage^2.*(2*pi*f*Cin).^2 * 10^18; % Sakmann-Neher p.112
+    % the filter
+    cutoff = filter;
+    factor = pi/1.8031;
+    [b,a] = besself(4,cutoff*factor);
+    h = freqs(b,a,x);
+    noise_model = 4*k*T*real(Y(x,Ra,Cm)*1e18.*h.*conj(h)) + ion*real(h.*conj(h)) + headstage(x).*real(h.*conj(h)) + + dielectric_loss(x).*real(h.*conj(h));
+    
+    y = noise_model;
+
+end
--- PoreView/plot_noise.m
+++ PoreView/plot_noise.m
@@ -72,7 +72,7 @@ function plot_noise(sigdata, trange)
         
         % scale x axis
         set(ax,'XLim',[1 f(imax)]);
-        set(gca,'FontSize',20)
+        set(gca,'FontSize',12)
         set(gca,'LooseInset',[0 0 0 0]) % the all-important elimination of whitespace!
         set(gca,'OuterPosition',[0 0 0.99 1]) % fit everything in there
         set(gcf,'Position',[100 500 750 500]) % size the figure
@@ -102,21 +102,26 @@ function plot_noise(sigdata, trange)
     k = 1.38 * 10^-23; % Boltzmann constant
     T = temp + 273.15; % absolute temperature in Kelvin
     Rf = 500e6; % feedback resistor in Axopatch with beta = 1 in whole cell mode
-    Cin = 4e-12; % headstage input capacitance is about 4pF
+    Cin = 4e-12; % headstage input capacitance is about 4pF (Sakmann and Neher say 15pF, p.112)
     V_headstage = 3e-9; % input voltage noise on headstage op-amp = 3nV/sqrt(Hz)
-    Ra = 2.5e7; % access resistance = rho/4*a (J.E. Hall, 1975), rho = 0.0895ohm*m for 1M KCl (http://www.sigmaaldrich.com/catalog/product/fluka/60131?lang=en&region=US), a = 1nm
-    Cm = 0.5e-12; % typical membrane capacitance 0.2pF
+    Ra = 3e7; % access resistance = rho/4*a (J.E. Hall, 1975), rho = 0.0895ohm*m for 1M KCl (http://www.sigmaaldrich.com/catalog/product/fluka/60131?lang=en&region=US), a = 1nm
+    Cm = 0.45e-12; % typical membrane capacitance 0.2pF
+    loss_tangent = 1; % Dave ref to http://pubs.acs.org/doi/pdf/10.1021/jp0680138
+    dielectric_loss = @(f) 8*pi*k*T*f*Cm*loss_tangent * 1e18;
     johnson = 4*k*T * ( 1/(R+2*Ra) + 1/Rf ) * 10^18;
     shot = 0; %2 * 1.602*1e-19 * abs(I) * 1e-12 * 10^18;
     ion = 1.12e-9 * (I*1e-12)^2 * (1e9)^2; % ion number fluctuations proportional to current squared (10/20/16 data for MspA from Oxford)
-    Y = @(f,Ra,Cm) sqrt(-1)*2*pi*f*Cin + 1./( 2*Ra + 1./( 1/R + sqrt(-1)*2*pi*f.*Cm ) ) + 1./Rf; % complex conductance (admittance)
-    headstage = V_headstage^2.*real(Y(f,Ra,Cm).*conj(Y(f,Ra,Cm))) * 10^18;
+    % Y = @(f,Ra,Cm) sqrt(-1)*2*pi*f*Cin + 1./( 2*Ra + 1./( 1/R + sqrt(-1)*2*pi*f.*Cm ) ) + 1./Rf; % complex conductance (admittance)
+    Y = @(f,Ra,Cm) 1./( 2*Ra + 1./( 1/R + sqrt(-1)*2*pi*f.*Cm ) ) + 1./Rf; % complex conductance (admittance)
+    % headstage = V_headstage^2.*real(Y(f,Ra,Cm).*conj(Y(f,Ra,Cm))) * 10^18;
+    headstage = @(f) V_headstage^2.*(2*pi*f*Cin).^2 * 10^18; % Sakmann-Neher p.112
     % the filter
     cutoff = 1e4;
-    factor = 2/1.8031;
-    [b,a] = besself(4,cutoff*2*factor);
+    factor = 2/1.8031 * pi/2.1;
+    [b,a] = besself(4,cutoff*factor);
     h = freqs(b,a,f);
-    noise_model = 4*k*T*real(Y(f,Ra,Cm)*1e18.*h.*conj(h)) + ion*real(h.*conj(h)) + shot*real(h.*conj(h)) + headstage.*real(h.*conj(h));
+    % noise_model = 4*k*T*real(Y(f,Ra,Cm)*1e18.*h.*conj(h)) + ion*real(h.*conj(h)) + shot*real(h.*conj(h)) + headstage.*real(h.*conj(h));
+    noise_model = 4*k*T*real(Y(f,Ra,Cm)*1e18.*h.*conj(h)) + ion*real(h.*conj(h)) + shot*real(h.*conj(h)) + headstage(f).*real(h.*conj(h)) + + dielectric_loss(f).*real(h.*conj(h));
     %display(' ')
     %display(['V = ' num2str(V,4) ' mV'])
     %display(['I = ' num2str(I,4) ' pA'])
@@ -136,7 +141,7 @@ function plot_noise(sigdata, trange)
     line([1 1e5],johnson*ones(1,2),'Color','k','LineStyle','--')
     hold on
     plot(f(1:imax)',dfft(1:imax,1));
-    line(f(1:imax),noise_model(1:imax),'Color','c','LineStyle','-')
+    
     try
         name = [sigdata.filename(65:68) '\_' sigdata.filename(70:71) '\_' sigdata.filename(73:74) '\_' sigdata.filename(76:end-4)];
         annotation('textbox', [0.75 0.9 0 0], 'String', name, 'FontSize', 20);
@@ -144,6 +149,19 @@ function plot_noise(sigdata, trange)
         % um
     end
     ylim([1e-12 1e-8])
+    
+    % also plot a mean smoothed version
+    x = f(2:imax);
+    y = dfft(2:imax,1);
+    xx = logspace(log10(min(x)),log10(5e4),2000);
+    subs = arrayfun(@(a) find(x(a)<=xx,1,'first'), 1:numel(x));
+    yy = accumarray(subs',y',[],@median);
+    xx = xx(yy~=0);
+    yy = yy(yy~=0);
+    plot(xx,yy)
+    
+    line(f(1:imax),noise_model(1:imax),'Color','c','LineStyle','-')
+    
     %xlim([1 10000])
 %     I_list = evalin('base','I');
 %     I_list = [I_list, I];
